cdcf2f4fc312016fc3eef2c62e1798ee2e9e9866
Leeni-Patidar <leenip04@gmail.com>
2025-08-06 16:05:00 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -22,7 +22,7 @@ const app = express()
 // ✅ Middleware
 app.use(
   cors({
-    origin: process.env.NODE_ENV === "production" ? ["https://yourdomain.com"] : ["http://localhost:5173"],
+    origin: process.env.NODE_ENV === "production" ? ["https://bloomingbasket-client.onrender.com"] : ["http://localhost:5173"],
     credentials: true,
   }),
 )
